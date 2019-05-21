3171210702447c0317635ea9442ce051c67143c2
jcamposforner <jcamposforner@gmail.com>
2019-05-21 18:52:50 +0200

    Middleware por rutas y por constructor

--- src/middleware/requestMiddlewar.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export const requestMiddlewar = (req: Request, res: Response, next: NextFunction) => {
-
-    res.status(200).send('a');
-}
--- src/middleware/requestMiddleware.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export const requestMiddleware = (req: Request, res: Response, next: NextFunction) => {
-
-    next();
-}
